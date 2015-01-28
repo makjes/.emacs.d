85793d48f72eae999727fcd8d3bb001198513312
Mats Kjesrud <matskje@student.matnat.uio.no>
2015-01-28 18:29:27 +0100

    Fixed keybindings

--- keybindings.el
+++ keybindings.el
@@ -10,11 +10,7 @@
 (define-key custom-bindings-map (kbd "C-x C-g s") 'magit-status)                    ; ref: packages/magit.el
 (define-key custom-bindings-map (kbd "C-x C-g l") 'gist-list)                       ; ref: packages/gist.el
 (define-key custom-bindings-map (kbd "C-x C-g u") 'gist-region-or-buffer-private)   ; ref: packages/gist.el
-(define-key custom-bindings-map (kbd "C-8")       'paredit-wrap-round)              ; Replace C-(. Sucks on NO-keyboards.
 
 (define-minor-mode custom-bindings-mode
   "A mode that activates custom-bindings."
   t nil custom-bindings-map)
-
-
-
