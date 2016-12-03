7038a832ed427c72975fa3594509794055cf549e
Mats Kjesrud <mats@nellon.no>
2016-12-03 16:30:18 +0100

    Started to get some sort of control

---
+++ init.el
@@ -0,0 +1,31 @@
+;; init.el --- Configuration of Emacs
+
+;; Added by Package.el.
+(package-initialize)
+
+(require 'org)
+(require 'ob-tangle)
+
+(setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
+(org-babel-load-file (expand-file-name "emacs.org" init-dir))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("db2ecce0600e3a5453532a89fc19b139664b4a3e7cbefce3aaf42b6d9b1d6214" default)))
+ '(hl-paren-colors
+   (quote
+    ("#B9F" "#B8D" "#B7B" "#B69" "#B57" "#B45" "#B33" "#B11")))
+ '(package-selected-packages
+   (quote
+    (magit monokai-theme arjen-grey-theme which-key ivy-hydra swiper use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
