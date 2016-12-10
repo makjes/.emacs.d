a5e01d7416423b79f858b0c53413d0559a7c1032
Mats Kjesrud <mats@nellon.no>
2016-12-11 00:02:32 +0100

    Cleaned up old impress crap

--- init.el
+++ init.el
@@ -7,25 +7,5 @@
 (require 'ob-tangle)
 
 (setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
-(org-babel-load-file (expand-file-name "test.org" init-dir))
+(org-babel-load-file (expand-file-name "emacs.org" init-dir))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("db2ecce0600e3a5453532a89fc19b139664b4a3e7cbefce3aaf42b6d9b1d6214" default)))
- '(hl-paren-colors
-   (quote
-    ("#B9F" "#B8D" "#B7B" "#B69" "#B57" "#B45" "#B33" "#B11")))
- '(package-selected-packages
-   (quote
-    (ox-jira htmlize ox-reveal ox-ioslide-helper ox-ioslide aggressive-indent auto-complete magit monokai-theme arjen-grey-theme which-key ivy-hydra swiper use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
