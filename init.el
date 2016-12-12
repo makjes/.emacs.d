c2481eade89d89110feea9e00855be3ded5e49c2
Mats Kjesrud <mats@nellon.no>
2016-12-12 23:09:09 +0100

    Major org-changes

--- init.el
+++ init.el
@@ -20,9 +20,10 @@
  '(hl-paren-colors
    (quote
     ("#B9F" "#B8D" "#B7B" "#B69" "#B57" "#B45" "#B33" "#B11")))
+ '(org-agenda-files nil)
  '(package-selected-packages
    (quote
-    (ox-jira htmlize ox-reveal ox-ioslide-helper ox-ioslide aggressive-indent auto-complete magit monokai-theme arjen-grey-theme which-key ivy-hydra swiper use-package))))
+    (yasnippet rainbow-delimiters paredit-everywhere paredit ox-jira htmlize ox-reveal ox-ioslide-helper ox-ioslide aggressive-indent auto-complete magit monokai-theme arjen-grey-theme which-key ivy-hydra swiper use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
