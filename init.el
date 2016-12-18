16fd3ab4ebf67ce6a38675568501c071e84c7b77
Mats Kjesrud <mats@nellon.no>
2016-12-18 22:58:40 +0100

    quite a bit of changes to my org-setup

--- init.el
+++ init.el
@@ -8,3 +8,17 @@
 
 (setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
 (org-babel-load-file (expand-file-name "emacs.org" init-dir))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet which-key use-package rainbow-delimiters paredit-everywhere ox-jira magit ivy-hydra htmlize counsel auto-complete arjen-grey-theme aggressive-indent))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
