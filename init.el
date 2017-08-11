e97ed4ef4748efcccd1ecb15484570d85d75403f
Mats Kjesrud <mats@nellon.no>
2017-08-11 09:38:58 +0200

    init.el cleanup

--- init.el
+++ init.el
@@ -1,11 +1,5 @@
-
-;; init.el --- Configuration of Emacs
-
-;; Added by Package.el.
 (package-initialize)
-
 (require 'org)
 (require 'ob-tangle)
-
 (setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
 (org-babel-load-file (expand-file-name "emacs.org" init-dir))
